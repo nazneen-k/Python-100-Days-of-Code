aa19b2726cc66b4c72c4267e7598131debfc947e
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-06-12 22:38:24 +0530

    Day 2

--- Day 2/datatypes.py
+++ Day 2/datatypes.py
@@ -1,3 +1,10 @@
 print("Hello"[4])
 
 print(123+ 852)
+
+a=123
+print(type(a))
+
+# Type Casting
+b=str(123)
+print(type(b))
---
+++ Day 2/operators.py
@@ -0,0 +1,16 @@
+print(3+5)
+print(7-1)
+print(3*6)
+print(6/3)
+print(2**2)  #Exponential
+
+
+# PEMDAS
+# Brackets
+# Exponents
+# Multiplication
+# Division 
+# Addition
+# Substraction
+
+
