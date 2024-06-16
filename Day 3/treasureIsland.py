2cf618543db1bc123eb081bb824a6c7c259657d6
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-06-16 23:47:50 +0530

    Day 3

---
+++ Day 3/treasureIsland.py
@@ -0,0 +1,5 @@
+print("Welcome to Tresure Island")
+print("Your mission is to find the tressure")
+choice1=input('You\'re at a crossroad, where do you want to go? Type "left" or "right".').lower
+if choice1=="right":
+    print("You fell into a hole. Game Over.")
