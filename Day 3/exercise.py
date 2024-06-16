4cf15c3af2e8b91948581089eba0e3784ff707b8
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-06-16 16:35:26 +0530

    Day 3

--- Day 3/exercise.py
+++ Day 3/exercise.py
@@ -1,2 +1,27 @@
 person1=input("Enter Person Name")
 person2=input("Enter Person Name")
+
+combinedNames=person1+person2
+loweredCombinedNames=combinedNames.lower()
+
+t=loweredCombinedNames.count('t')
+r=loweredCombinedNames.count('r')
+u=loweredCombinedNames.count('u')
+e=loweredCombinedNames.count('e')
+
+firstDigit=t+r+u+e
+
+l=loweredCombinedNames.count('l')
+o=loweredCombinedNames.count('o')
+v=loweredCombinedNames.count('v')
+e=loweredCombinedNames.count('e')
+
+secondDigit=l+o+v+e
+
+score=int(str(firstDigit)+str(secondDigit))
+if(score<10) or (score>90):
+    print(f'Your score is {score}, you go together like coke and mentos')
+elif (score>=40) and (score<=50):
+    print(f'Your score is {score}, you are alright together')
+else:
+    print("The Score is:",score)
