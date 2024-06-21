5b649ae0c62b07bb3d972559a22abf6f1c6fd4b3
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-06-21 23:09:58 +0530

    Day 5

--- Day 5/passwordGenerator.py
+++ Day 5/passwordGenerator.py
@@ -12,3 +12,44 @@ nrLetters=int(input("How many leters would you lime in your password?\n"))
 nrSymbols=int(input("How many symbols wold you like \n"))
 nrNumbers=int(input("How many numbers would you like?"))
 
+# Easy Level
+password=""
+
+for char in range(1,nrLetters+1):
+    randomChar=random.choice(letters)
+    password+=randomChar
+    print(password)
+
+for char in range(1,nrSymbols+1):
+    password+=random.choice(symbols)
+
+for char in range(1,nrNumbers):
+    password+=random.choice(numbers)
+
+print("Your Password is:",password)
+
+
+# Hard Level
+
+passwordList=[]
+
+for char in range(1,nrLetters+1):
+    randomChar=random.choice(letters)
+    passwordList+=randomChar
+    
+
+for char in range(1,nrSymbols+1):
+    passwordList+=random.choice(symbols)
+
+for char in range(1,nrNumbers+1):
+    passwordList+=random.choice(numbers)
+
+print("Your Password is:",passwordList)
+
+random.shuffle(passwordList)
+print(passwordList)
+
+finalPassword=''
+for char in passwordList:
+    finalPassword+=char
+print("Your Password Generated is:",finalPassword)
