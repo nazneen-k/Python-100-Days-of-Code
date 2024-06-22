d8bbf3238d725a900cdc9df068439896899e09f2
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-06-22 13:52:53 +0530

    Day 6

---
+++ Day 6/functions.py
@@ -0,0 +1,11 @@
+#A function is defined using the def keyword, followed by the function name, parentheses (which can include parameters), and a colon. The body of the function is indented.
+
+# Parameters and Arguments
+# Parameters: Variables listed inside the parentheses in the function definition.
+# Arguments: Values passed to the function when it is called.
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
