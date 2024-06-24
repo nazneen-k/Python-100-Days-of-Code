94f2237f7a3a8831235df2f80aeb0c7045d62851
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-06-24 21:29:43 +0530

    Day 7

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -1,1 +1,22 @@
 word_list=["ardvark","baboon","camel"]
+
+import random
+chose_word= random.choice(word_list)
+
+# print(chose_word)
+
+display=[]
+# for letter in chose_word:
+word_length=len(chose_word)
+for _ in range(word_length):
+    display+="_"
+print(display)
+
+guess=input("Guess a letter:").lower()
+
+for position in range(word_length):
+    letter=chose_word[position]
+    if letter  == guess:
+        display[position]=letter
+
+print(display)
