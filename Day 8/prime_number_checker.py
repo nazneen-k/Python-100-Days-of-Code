6ce72aa5ad8e3bee0aec7b034c5ef7a1c20680f6
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-06-30 10:23:50 +0530

    Prime Number Checker

--- Day 8/prime_number_checker.py
+++ Day 8/prime_number_checker.py
@@ -1,2 +1,15 @@
+n=int(input("Enter a number to check whether it is a Prime Number or Not"))
+
+
+
 def prime_checker(number):
     is_prime=True
+    for i in range(2,number):
+        if number %i==0:
+            is_prime=False
+    if is_prime:
+        print("It is a Prime Number")
+    else:
+        print("Its not a Prime Number")
+
+prime_checker(number=n)
