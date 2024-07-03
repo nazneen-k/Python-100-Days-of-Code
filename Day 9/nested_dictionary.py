5c15778b0b836de6d518fa0fe303dc2f168ba4f6
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-07-03 23:01:22 +0530

    Day 9

---
+++ Day 9/nested_dictionary.py
@@ -0,0 +1,23 @@
+capitals={
+    "France":"Paris",
+    "Germary":"Berlin"
+}
+
+# Nesting a List in a Dictionary
+travel_log = {
+    "France":{"cities_visited":["Paris","Lille"], "total_visits":12},
+    "German":["Berlin","Hamburg","Stuttgart"]
+}
+
+# Nesting Dictionary in a List
+
+travel_log=[
+    {
+        "country":"France",
+        "cities_visited":["paris","Lille","Dijon"],"total_visits":12
+    },
+    {
+        "country":"Germany",
+        "cities_visited":["Berlin","Humburg","Stuttgarl"]
+    }
+]
