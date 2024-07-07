168f777a2824eea84caf0a5b4fef3a9f08b09f5f
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-07-07 21:47:42 +0530

    Day 10

---
+++ Day 10/functions_with_outputs.py
@@ -0,0 +1,8 @@
+# Functions with Outputs
+
+def format_name(f_name,l_name):
+    print(f_name.title())
+    print(l_name.title())
+# title() ---> Returns a version of the string where each word  is titlecased. More specifically, word start with uppercased characters and all remaining cased characters have lower case.
+
+format_name("anGela","ANGELA")
