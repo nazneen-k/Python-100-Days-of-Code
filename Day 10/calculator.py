b52d440fd415e36e5f1f80969817b529b37e7f0e
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-07-09 19:52:10 +0530

    Day 10

--- Day 10/calculator.py
+++ Day 10/calculator.py
@@ -1,3 +1,5 @@
+from art import logo
+
 # Add
 def add(n1,n2):
     return n1+n2
@@ -18,12 +20,25 @@ operations= {
     "/":divide
 }
 
-num1=int(input("What's the first number?"))
-num2=int(input("What's the second number?"))
-for symbol in operations:
-    print(symbol)
-operation_symbol= input("Pick an operation from the line above:")
-calculation_function = operations[operation_symbol]
-answer=calculation_function(num1,num2)
+def calculator():
+    print(logo)
+    num1=float(input("What's the first number?"))
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+    for symbol in operations:
+        print(symbol)
+    should_continue=True
+
+    while should_continue:
+        operation_symbol= input("Pick an operation: ")
+        num2=float(input("What's the next Number? "))
+        calculation_function = operations[operation_symbol]
+        answer=calculation_function(num1,num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit : ") == "y":
+            num1= answer
+        else: 
+            should_continue = False
+            
+
+calculator()
