e9952efc818ac5f6c50aaa4c114a60c043272a1f
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-07-10 19:47:50 +0530

    Day 11

--- Day 11/blackjack_project.py
+++ Day 11/blackjack_project.py
@@ -6,19 +6,29 @@ def deal_card():
     card=random.choice(cards)
     return card
 
-user_cards=[]
-computer_cards=[]
-
-for _ in range(2):
-    # new_card=deal_card()
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
-
 
 def calculate_score(cards):
+    # Takes a list of cards and retuns the score calculated from the cards
     if sum(cards) == 21 and len(cards)==2:
         return 0
     if 11 in cards and sum(cards)>21:
         cards.remove(11)
         cards.append()
     return sum(cards)
+
+user_cards=[]
+computer_cards=[]
+is_game_over = False
+
+for _ in range(2):
+    # new_card=deal_card()
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+user_score=calculate_score(user_cards)
+computer_score=calculate_score(computer_cards)
+print(f"Your cards: {user_cards}, current score: {user_score}")
+print(f"Computer's first card: {computer_cards[0]}")
+
+if user_score == 0 or computer_score==0 or user_score >21:
+    is_game_over=True
