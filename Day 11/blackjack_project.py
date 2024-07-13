fcc4da10901c3ec57f7ae46031b213584cf303cf
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-07-13 22:31:24 +0530

    Day 11

--- Day 11/blackjack_project.py
+++ Day 11/blackjack_project.py
@@ -70,5 +70,5 @@ def playgame():
     print(compare(user_score,computer_score))
 
 
-while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") =="y":
+while input("Do you want to play a game of Blackjack? Type 'y' Or 'n': ") =="y":
     playgame()
