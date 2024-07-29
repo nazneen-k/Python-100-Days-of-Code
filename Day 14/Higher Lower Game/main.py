baad9f3426bdc40a159b1cec5de45ac310a8c0e6
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-07-29 19:47:19 +0530

    Day 14

--- Day 14/Higher Lower Game/main.py
+++ Day 14/Higher Lower Game/main.py
@@ -1,2 +1,31 @@
 from art import logo
+from data import data
+import random
+
+# Display Art 
 print(logo)
+
+
+def format_data(account):
+    """Format the account data into printable format"""
+    account_name = accou["name"]
+    account_desc = accou["description"]
+    account_country = accou["country"]
+
+    print(f"{account_name}, a {account_desc} from {account_country}")
+
+# Generate a random acccount from the game data
+account_a= random.choice(data)
+account_b= random.choice(data)
+if account_a ==  account_b:
+    account_b=random.choice(data)
+
+print(f"Compare A : {format_data(account_a)}")
+print(f"Companre B : {format_data(account_b)}")
+
+
+
+
+
+
+
