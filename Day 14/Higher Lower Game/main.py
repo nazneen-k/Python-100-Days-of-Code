6c7ec387c2b3ef4b98c1aabb1796fe8560809806
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-07-29 20:42:18 +0530

    Day 14

--- Day 14/Higher Lower Game/main.py
+++ Day 14/Higher Lower Game/main.py
@@ -1,4 +1,4 @@
-from art import logo
+from art import logo, vs
 from data import data
 import random
 
@@ -7,12 +7,15 @@ print(logo)
 
 
 def format_data(account):
-    """Format the account data into printable format"""
-    account_name = accou["name"]
-    account_desc = accou["description"]
-    account_country = accou["country"]
+    """Takes the account data and returns the printable format"""
+    account_name = account["name"]
+    account_desc = account["description"]
+    account_country = account["country"]
 
-    print(f"{account_name}, a {account_desc} from {account_country}")
+    return (f"{account_name}, a {account_desc} from {account_country}")
+
+def check_answer(guess, a_followers,b_followers):
+    """Take the user guess and  follower counts and returns if they got it right"""
 
 # Generate a random acccount from the game data
 account_a= random.choice(data)
@@ -21,7 +24,13 @@ if account_a ==  account_b:
     account_b=random.choice(data)
 
 print(f"Compare A : {format_data(account_a)}")
+print(vs)
 print(f"Companre B : {format_data(account_b)}")
+
+guess=input("Who has more followers? Type  'A' or 'B': ").lower()
+
+a_follower_count = account_a["follower_count"]
+b_follower_count = account_b["follower_count"]
 
 
 
