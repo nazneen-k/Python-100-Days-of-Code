d69138e50442977298d4c2fd72bc191559e210a8
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-09-29 22:50:28 +0530

    Day 15

--- Day 15/main.py
+++ Day 15/main.py
@@ -55,7 +55,6 @@ is_on= True
 while is_on:
     choice = input("What would you like>? (espresso/latte/cuppuccino)")
     if choice=="off":
-        
         is_on = False
         
     elif choice =="report":
