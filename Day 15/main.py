ad136135527a992e4ab0973c603a7397aa0969ab
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-10-16 12:24:28 +0530

    Day 15

--- Day 15/main.py
+++ Day 15/main.py
@@ -50,6 +50,25 @@ def process_coins():
 
 def is_transaction_successful(money_recieved, drink_cost):
     """Return True when the Payment is accepted, or False if the money is insufficient """
+    if money_recieved>= drink_cost:
+        change= round(money_recieved-drink_cost,2)
+        print(f"Here is {change} in change")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry!! That's not enough money. Monet refunded.")
+        return False
+    
+
+def make_coffee(drink_name,order_ingredients):
+    """Deduct the required ingredients from the resources"""
+    for item in order_ingredients:
+        resources[item]-=order_ingredients[item]
+    print(f"Here is your {drink_name}☕")
+    
+
+
 
 is_on= True
 while is_on:
@@ -69,6 +88,9 @@ while is_on:
         
         if is_resource_sufficient(drink["ingredients"]):
             payment=process_coins()
+            if is_transaction_successful(payment,drink["cost"]):
+                make_coffee(choice,drink["ingredients"])
+
 
         
 
