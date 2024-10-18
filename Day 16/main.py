8c6c1757fb7118e5e6f440a32a0ef64189755002
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-10-18 15:56:57 +0530

    Day 16

--- Day 16/main.py
+++ Day 16/main.py
@@ -1,1 +1,23 @@
 # Object Oriented Programming
+
+
+# import another_module
+# print(another_module.another_variable)
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+print(timmy)
+timmy.shape("turtle")
+timmy.color("green")
+timmy.forward(100)
+timmy.left(200)
+timmy.forward(100)
+timmy.right(100)
+timmy.forward(100)
+
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
