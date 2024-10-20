d44109ddd7b3da2ca19c6a1d6a6c7bbdaab7265d
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-10-20 11:50:31 +0530

    Day 16

--- Day 16/main.py
+++ Day 16/main.py
@@ -4,21 +4,34 @@
 # import another_modul e
 # print(another_module.another_variable)
 
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
+# timmy = Turtle()
 
-print(timmy)
-timmy.shape("turtle")
-timmy.color("green")
-timmy.forward(100)
-timmy.left(200)
-timmy.forward(100)
-timmy.right(100)
-timmy.forward(100)
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("green")
+# timmy.forward(100)
+# timmy.left(200)
+# timmy.forward(100)
+# timmy.right(100)
+# timmy.forward(100)
 
 
-my_screen = Screen()
-print(my_screen.canvheight)
+# my_screen = Screen()
+# print(my_screen.canvheight)
 
-my_screen.exitonclick()
+# my_screen.exitonclick()
+
+
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+table.align="l"
+
+print(table)
+table.align="r"
+print(table)
