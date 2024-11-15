5f053a6ee96f1cea3a61c4b7e3bc166c22587784
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-11-16 00:07:40 +0530

    Day 17

--- Day 17/Quiz Game/quiz_brain.py
+++ Day 17/Quiz Game/quiz_brain.py
@@ -5,6 +5,7 @@ class QuizBrain:
         self.question_list=q_list
 
     def next_question(self):
-        current_question=self.question_list(self.question_number)
-        input(f"Q.{self.question_number}:{current_question.text}{True/False}")
+        current_question=self.question_list[self.question_number]
+        self.question_number+=1
+        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
 
