afd7eeb2ef0dc0f28172d46233c9adbbe9fb2670
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-12-01 13:16:29 +0530

    Day 18

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,13 +1,23 @@
 from turtle import Turtle, Screen
+# keyword Module_name keyword Thing_in_module
 
 timmy_the_turtle = Turtle()
 
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("red")
 
-for i in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(90)
+# Drawing a box
+# for i in range(4):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.left(90)
+
+# Drawing Dashed Lines
+for tim in range(10):
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.penup()
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.pendown()
+
 
 screen = Screen()
 screen.exitonclick()
