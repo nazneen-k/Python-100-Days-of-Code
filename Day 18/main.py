863fd283c344aac10c1236045c9ed84f9ae0b0cd
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-12-02 22:49:55 +0530

    Day 18

--- Day 18/main.py
+++ Day 18/main.py
@@ -19,7 +19,32 @@ for tim in range(10):
     timmy_the_turtle.pendown()
 
 
+
+
+
+# Drawing different shapes
+
+# num_size=5
+
+# for _ in range(num_size):
+#     angle=360/num_size
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(angle)
+
+
+
+
+
+def draw_shape(num_sides):
+    angle=360/num_sides
+    for _ in range(num_sides):
+        timmy_the_turtle.forward(100)
+        timmy_the_turtle.right(angle)
+
+for shape_side_n in range(3,11):
+    draw_shape(shape_side_n)
+
+
+
 screen = Screen()
 screen.exitonclick()
-
-
