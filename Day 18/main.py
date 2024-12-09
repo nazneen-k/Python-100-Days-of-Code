3ac682f355c92b171ebb57700ff6e8e6bff6ce52
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-12-09 22:11:48 +0530

    Day 18

--- Day 18/main.py
+++ Day 18/main.py
@@ -34,17 +34,25 @@ for tim in range(10):
 
 
 colours=["CornflowerBlue","DarkOrchid","IndianRed","DeepSkyBlue","LightSeaGreen","wheat","SlateGray","SeaGreen"]
-
-def draw_shape(num_sides):
-    angle=360/num_sides
-    for _ in range(num_sides):
-        timmy_the_turtle.forward(100)
-        timmy_the_turtle.right(angle)
+directions=[0,90,180,270]
 
 
-for shape_side_n in range(3,11):
-    timmy_the_turtle.color(random.choice(colours))
-    draw_shape(shape_side_n)
+# def draw_shape(num_sides):
+#     angle=360/num_sides
+#     for _ in range(num_sides):
+#         timmy_the_turtle.forward(100)
+#         timmy_the_turtle.right(angle)
+
+
+# for shape_side_n in range(3,11):
+#     timmy_the_turtle.color(random.choice(colours))
+#     draw_shape(shape_side_n)
+
+
+for _ in range(200):
+    timmy_the_turtle(30)
+    timmy_the_turtle(random.choice(directions))
+
 
 
 screen = Screen()
--- Day 18/tempCodeRunnerFile.py
+++ Day 18/tempCodeRunnerFile.py
@@ -1,11 +1,11 @@
-from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
-screen = Screen()
-screen.exitonclick()
+# def draw_shape(num_sides):
+#     angle=360/num_sides
+#     for _ in range(num_sides):
+#         timmy_the_turtle.forward(100)
+#         timmy_the_turtle.right(angle)
 
 
+# for shape_side_n in range(3,11):
+#     timmy_the_turtle.color(random.choice(colours))
+#     draw_shape(shape_side_n)
